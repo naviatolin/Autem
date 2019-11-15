9e3ce9b767cc855c3b821b9d21cce98795557ba3
Pranavi Boyalakuntla <naviatolin@gmail.com>
2019-11-15 16:12:06 -0500

    add from friday's work session

--- app/database.py
+++ app/database.py
@@ -8,5 +8,15 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# from flask_login import UserMixin
+
+# class User(UserMixin, db.Model)
