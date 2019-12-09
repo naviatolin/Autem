a0e2f421d13fa6e60ad2eabfc7357ada18d68e39
Maya Al-Ahmad <mahmad1@olin.edu>
2019-12-09 17:52:58 -0500

    tiref

--- application.py
+++ application.py
@@ -3,6 +3,6 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.01'
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
     PORT = int(os.environ.get('PORT', 5000)) 
     app.run(host=HOST, port=PORT, debug=True)
