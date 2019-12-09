9dd08c5e3a81cb0f594371a27074af81fc453a47
Maya Al-Ahmad <mahmad1@olin.edu>
2019-12-09 17:42:10 -0500

    tiref

--- application.py
+++ application.py
@@ -1,3 +1,7 @@
 """ Start Application """ 
 from app import app
 
+if __name__ == '__main__':
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.01'
+    PORT = int(os.environ.get('PORT', 5000)) 
+    app.run(host=HOST, port=PORT, debug=True)
