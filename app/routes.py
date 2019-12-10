06754147e54bb40dba25c5bc1b9341af3cfff5d1
Maya Al-Ahmad <mahmad1@olin.edu>
2019-12-10 08:32:03 -0500

    create_Task created

--- app/routes.py
+++ app/routes.py
@@ -49,6 +49,32 @@ def create_event(start_time_str, summary, duration=1, description=None, location
             'RRULE:FREQ=WEEKLY',
         ],
     }
+
+def create_task(start_time_str, summary, duration=1, description=None, location=None):
+    matches=list(datefinder.find_dates(start_time_str))
+    if len(matches):
+        start_time = matches[0]
+        end_time = start_time + timedelta(hours=duration)
+        
+    event = {
+        'summary': summary,
+        'location': location,
+        'description': description,
+        'start':{
+            'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'America/New_York'
+        },
+        'end': {
+            'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'America/Chicago'
+        },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
     return service.events().insert(calendarId='primary', body=event).execute()
 
 @app.route('/')
