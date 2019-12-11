896437cc1c1f67b9465609f11279dc86bc2a1af4
Pranavi Boyalakuntla <naviatolin@gmail.com>
2019-12-11 00:27:22 -0500

    Add more to the scheduling algorithm

--- test_task_seperating.py
+++ test_task_seperating.py
@@ -2,18 +2,12 @@ import json
 from datetime import datetime, timedelta
 import math
 import pickle
-from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm, TaskForm, EventForm, SurveyForm
-import os
 import apiclient
 from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle 
 import json
 from datetime import datetime, timedelta
 import datefinder
-import requests
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 credentials = pickle.load(open("token.pkl", "rb"))
 service = apiclient.discovery.build("calendar", "v3", credentials=credentials)
@@ -28,7 +22,10 @@ def break_up_tasks():
         task_database = json.load(file)
     
     with open('user_preference.json', 'rb') as file:
-        task_database = json.load(file)
+        user_preferences_database = json.load(file)
+    
+    start_day_time = int(user_preferences_database["start_day_hour"])
+    end_day_time = int(user_preferences_database["end_day_hour"]) + 12
         
     unplaced_tasks = {}
     for key in task_database:
@@ -36,7 +33,6 @@ def break_up_tasks():
             unplaced_tasks[key] = task_database[key]
     
     order_keys_date = sorted(unplaced_tasks, key=lambda x: unplaced_tasks[x]['due_date'])
-    print(order_keys_date)
     date_format = "%Y-%m-%d"
 
     for key in order_keys_date:
@@ -45,20 +41,36 @@ def break_up_tasks():
         today = datetime.today()
         due_date = datetime.strptime(unplaced_tasks[key]["due_date"], date_format)
         number_of_days_to_complete = (due_date - today).days
+
         if number_of_days_to_complete > 0:
             chunks_per_day = math.ceil(float(number_of_chunks)/number_of_days_to_complete)
-            for chunk in chunks_per_day:
-                page_token = None
-                while True:
-                    events = service.events().list(calenderId='primary', pageToken=page_token).execute()
-                    for event in events['items']:
-                        print(event["summary"])
-                    page_token = events.get('nextPageToken')
-                    if not page_token:
-                        break
+
+            available_days = []
+            current_day = datetime.today() + timedelta(days=1)
+            for day in range(0, number_of_days_to_complete):
+                available_days.append(current_day)
+                current_day = current_day+ timedelta(days=1)
+            
+
+            for day in range(0, len(available_days)):
+                for chunk in range(0, chunks_per_day):
+                    minute = 0
+
+                    start_date = datetime(available_days[day].year, available_days[day].month, available_days[day].day, start_day_time, minute, 0).isoformat() + 'Z'
+
+                    end_date = datetime(available_days[day].year, available_days[day].month, available_days[day].day, end_day_time, minute, 0).isoformat() + 'Z'
+                    events = get_tasks_day(start_date, end_date)
+                    if len(events) is not 0:
+                        minute = minute + 15
         else:
             continue
 
+def get_tasks_day(start_date, end_date):
+    with open('user_preference.json', 'rb') as file:
+        user_preferences_database = json.load(file)
 
+    eventsResult = service.events().list(calendarId=calendar_id, timeMin=start_date, timeMax=end_date, singleEvents=True, orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+    return events
 
 break_up_tasks()
